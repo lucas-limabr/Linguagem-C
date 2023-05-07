4acc1a8b177bd682c1a35d0f24034b129067827b
lucas-limabr <lucaslimamg28@gmail.com>
2023-05-06 23:36:42 -0300

    add exercicio de concatenacao

---
+++ Concatenando strings com strncat.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+    char str1[30] = "Curso ";
+    char str2[20] = "de programacao em C ";
+
+    strncat(str1, str2,15);
+
+    printf("str1 = %s \n", str1);
+
+    system("pause");
+
+}
