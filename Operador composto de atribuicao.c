95954edec43666bdb58ffb2e8fcb1f17d6909580
lucas-limabr <lucaslimamg28@gmail.com>
2023-05-25 23:05:24 -0300

    add exercício

---
+++ Operador composto de atribuicao.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+    int num = 10;
+
+    num += 5;
+    printf("O valor da variavel = %d \n", num);
+
+    num *= 2;
+    printf("O valor da variavel = %d \n", num);
+
+    num /= 10;
+    printf("O valor da variavel = %d \n", num);
+
+    system("pause");
+}
