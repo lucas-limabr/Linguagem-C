87a26abc827456bfdb56001c033321e031439aa2
lucas-limabr <lucaslimamg28@gmail.com>
2023-06-13 21:49:51 -0300

    add exercício de struct

--- Aplicacao financeira/main.c
+++ Aplicacao financeira/main.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<locale.h>
 
-int main()
+struct estrutura
 {
     float valor_inicial, taxa;
     int dias;
+};
+
+typedef struct estrutura Aplicacao;
+
+float calculoRendimento(Aplicacao aplicacao);
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    Aplicacao aplicacao;
 
     printf("Informe o valor do depósito inicial: ");
-    scanf("%f", &valor_inicial);
+    scanf("%f", &aplicacao.valor_inicial);
 
-    printf("Qual é o valor da taxa de juros fixa? ");
-    scanf("%f", &taxa);
+    printf("Qual é o valor da taxa de juros fixa em porcentagem? ");
+    scanf("%f", &aplicacao.taxa);
 
     printf("Por quantos dias o valor será aplicado? ");
-    scanf("%d", &dias);
+    scanf("%d", &aplicacao.dias);
 
-    printf("O saldo final da aplicação finaNceira é de %.2f R$", aplicacaoFinanceira(valor_inicial, taxa, dias));
+    if(calculoRendimento(aplicacao)){
+    printf("\nO saldo final da aplicação financeira é de %.2f R$\n", calculoRendimento(aplicacao));
+    }
+    else{
+        printf("\nErro, algum dado está com valor NEGATIVO\n");
+    }
 }
 
-float aplicacaoFinanceira(float valor_inicial, float taxa, int dias){
+float calculoRendimento(Aplicacao aplicacao)
+{
 
+    //convertendo a taxa para um valor decimal
+    float valor_final, taxa_decimal = ((aplicacao.taxa + 100) / 100);
+    int cont = 1;
+
+    if(aplicacao.dias <= 0 || aplicacao.taxa <= 0 || aplicacao.valor_inicial <= 0)
+    {
+        return valor_final = 0;
+    }
+    else
+    {
+
+        valor_final = aplicacao.valor_inicial * taxa_decimal;
+
+        while(cont < aplicacao.dias)
+        {
+            valor_final *= taxa_decimal;
+            cont++;
+        }
+        return valor_final;
+    }
 }
