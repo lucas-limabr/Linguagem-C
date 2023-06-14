7805470d08499a1faf8d68ba3ed9cbd778e46c60
lucas-limabr <lucaslimamg28@gmail.com>
2023-06-14 17:36:49 -0300

    update

--- Idade alcançada/main.c
+++ Idade alcançada/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<locale.h>
+
+struct Pessoa{
+    float altura, crescimento;
+};
+
+int calculaAnos(struct Pessoa chico, struct Pessoa ze);
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+   setlocale(LC_ALL, "");
+
+   struct Pessoa chico;
+   chico.altura = 1.5;
+   chico.crescimento = 0.02;
+
+   struct Pessoa ze;
+   ze.altura = 1.1;
+   ze.crescimento = 0.03;
+
+   printf("Levará %d anos para que a altura de Ze ultrapasse a altura de Chico", calculaAnos(chico, ze));
+}
+
+int calculaAnos(struct Pessoa chico, struct Pessoa ze){
+    int qtd_anos = 0;
+
+    while(ze.altura <= chico.altura){
+       ze.altura += 0.03;
+       chico.altura += 0.02;
+       qtd_anos++;
+    }
+
+    return qtd_anos;
 }
